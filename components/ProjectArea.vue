834deef272d8c82c7fb3376e58ee467212e8c5a2
Huxinmeng <2234373299@qq.com>
2022-10-19 19:03:10 +0800

    项目

--- components/ProjectArea.vue
+++ components/ProjectArea.vue
@@ -10,18 +10,22 @@
       </div>
       <div class="row project-active">
         <swiper :options="swiperOptions">
-          <swiper-slide>
-          <div class="project-item mt-30">
-            <div class="project-thumb">
-              <img src="https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/spider.png" alt="projects">
+          <swiper-slide v-for="it in projectList" :key="it.id">
+            <div class="project-item mt-30">
+              <div class="project-thumb">
+                <img
+                  :src="it.project_img_url"
+                  alt="projects"
+                />
+              </div>
+              <div class="project-content">
+                <a href="#" v-if="it.detail_page_url==null"><span>{{it.project_name}}</span></a>
+                 <a v-else :href="it.project_url" ><span>{{it.project_name}}</span></a>
+                <p>recent project</p>
+              </div>
             </div>
-            <div class="project-content">
-              <a href="#"><span>电商爬虫及可视化分析</span></a>
-              <p>recent project</p>
-            </div>
-          </div>
           </swiper-slide>
-          <swiper-slide>
+          <!-- <swiper-slide>
           <div class="project-item mt-30">
             <div class="project-thumb">
               <img src="https://hxm-1314321198.cos.ap-nanjing.myqcloud.com/car.png" alt="projects">
@@ -64,7 +68,7 @@
               <p>recent project</p>
             </div>
           </div>
-          </swiper-slide>
+          </swiper-slide> -->
           <div class="swiper-pagination" slot="pagination"></div>
         </swiper>
       </div>
@@ -73,55 +77,66 @@
 </template>
 
 <script>
-  import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
-  import 'swiper/css/swiper.css';
+import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 
-  export default {
-    name: "ProjectArea",
-    components: {
-      Swiper,
-      SwiperSlide
-    },
-    directives: {
-      swiper: directive
-    },
-    data() {
-      return {
-        swiperOptions: {
-          slidesPerView : 3,
-          loop: true,
-          speed: 1000,
-          spaceBetween : 30,
-          autoplay: {
-            delay: 3000,
-            disableOnInteraction: false
+export default {
+  name: "ProjectArea",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  directives: {
+    swiper: directive,
+  },
+  data() {
+    return {
+      projectList:[],
+      swiperOptions: {
+        slidesPerView: 3,
+        loop: true,
+        speed: 1000,
+        spaceBetween: 30,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true,
+        },
+        // Responsive breakpoints
+        breakpoints: {
+          1024: {
+            slidesPerView: 3,
           },
-          pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
+          768: {
+            slidesPerView: 2,
           },
-          // Responsive breakpoints
-          breakpoints: {
-            1024:{
-              slidesPerView : 3
-            },
-            768:{
-              slidesPerView : 2
-            },
-            640:{
-              slidesPerView : 1
-            },
-            320:{
-              slidesPerView : 1
-            }
-          }
-        }
-      }
+          640: {
+            slidesPerView: 1,
+          },
+          320: {
+            slidesPerView: 1,
+          },
+        },
+      },
+    };
+  },
+  created() {
+    this.getProjectData();
+  },
+  methods: {
+    async getProjectData() {
+      const project = await this.$axios.$get("/index/latest-project");
+      // console.log(project["data"]);
+      if (project["code"] != 0) return;
+      this.projectList = project["data"];
     },
-  }
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
